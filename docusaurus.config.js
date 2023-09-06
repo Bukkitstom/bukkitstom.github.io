eaee2902681814badafdc14e97281d6bca5d6096
Window5 <spam.window5.spam@gmail.com>
2023-09-06 21:06:43 +0200

    oops

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'bukkitstom', // Usually your GitHub org/user name.
-  projectName: 'bukkitstom', // Usually your repo name.
+  projectName: 'bukkitstom.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
